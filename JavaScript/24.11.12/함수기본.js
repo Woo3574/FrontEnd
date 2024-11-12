93637364f3c11e9fac67e94d5eae56921ba042c5
Woo3574 <rlaxodn0429@gmail.com>
2024-11-12 11:36:44 +0900

    24.11.12

--- JavaScript/24.11.12/함수기본.js
+++ JavaScript/24.11.12/함수기본.js
@@ -49,3 +49,60 @@ function add(a, b = 10, c = 20) {
 }
 
 console.log(add(1));
+
+// 스코프 : 자바스크립트는 함수 스코프와 블록 스코프 방식으로 나누어 짐
+// 함수 스코프 : 함수에서 정의한 블록문만 스코프의 유효 범위로 인식하는 방식
+// let a = 10; // 전역 변수
+function sum2() {
+  let a = 10; // 지역 변수
+  console.log(`함수 내부 : ${a}`);
+}
+
+sum2(); // 함수를 호출해줘야 실행됨
+console.log(`함수 외부 : ${a}`);
+
+// 블록 스코프 : 자바스크립트는 함수 스코프 기반의 언어였지만,
+// ES6에서 let, const키워드 추가 되면서 블록 스코프도 지원
+let b = 10;
+{
+  let c = 20;
+  console.log(`코드 블록 내부 : ${b}`);
+  console.log(`코드 블록 내부 : ${c}`);
+}
+
+console.log(`코드 블록 외부 : ${b}`);
+console.log(`코드 블록 외부 : ${c}`);
+
+// let은 재할당이 가능 const는 재할당 불가능,상수값 선언시 사용
+//var은 함수 스코프까지 let, const는 블록스코프까지 제한된다
+
+//참조 우선순위 : let, const 키워드는 같은 식별자의 중복 선언이 불가능
+let d = 10;
+const e = 20;
+
+{
+  let d = 20;
+  const e = 30;
+  console.log(`블록 내부 : ${d}`);
+  console.log(`블록 내부 : ${e}`);
+}
+
+console.log(`블록 외부 : ${d}`);
+console.log(`블록 외부 : ${e}`);
+
+// 즉시 실행 함수 : 함수를 정의하자마자 바로 실행하는 함수, 전역 변수의 오염을 막기위해서
+(function () {
+  // 화면 초기화 루틴 수행
+  // 통신 모듈 초기화
+  initScreen();
+  // console.log("화면 초기화 루틴 실행.....");
+  console.log("통신 모듈 초기화.....");
+  console.log("키오스크 동작 준비 완료....");
+})();
+
+function initScreen() {
+  console.log("화면 초기화 루틴 실행.....");
+}
+
+// Callback 함수 : 함수의 인자로 다른 함수를 전달하여, 그함수가 실행될 때 호출되는 함수
+// 주로 비동기 처리나 사용자 이벤트 처리에 사용
