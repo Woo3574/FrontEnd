12cf575a1a9ca342787792fb7e5702062f291a95
Woo3574 <rlaxodn0429@gmail.com>
2024-11-15 15:06:18 +0900

    24.11.15

---
+++ JavaScript/24.11.15/json실습.js
@@ -0,0 +1,53 @@
+// JSON 데이터
+const jsonData = `
+[
+    {
+        "name": "안유진",
+        "age": 20,
+        "isStudent": false,
+        "courses": ["음악", "댄스", "연기"],
+        "address": {
+            "city": "서울",
+            "zipcode": "12345"
+        }
+    },
+    {
+        "name": "가을",
+        "age": 20,
+        "isStudent": false,
+        "courses": ["음악", "댄스", "연기"],
+        "address": {
+            "city": "서울",
+            "zipcode": "12345"
+        }
+    },
+    {
+        "name": "레이",
+        "age": 19,
+        "isStudent": true,
+        "courses": ["음악", "일본어", "연기"],
+        "address": {
+            "city": "서울",
+            "zipcode": "12345"
+        }
+    }
+]
+`;
+
+// JSON 문자열을 JavaScript 객체로 변환 (역직렬화)
+const jsonObject = JSON.parse(jsonData);
+console.log("객체로 변환된 데이터:", jsonObject);
+const htmlContent = jsonObject
+  .map(
+    (person) => `
+  <div class="person">
+      <h2>${person.name}</h2>
+      <p>나이: ${person.age}</p>
+      <p>학생 여부: ${person.isStudent ? "예" : "아니오"}</p>
+      <p>과목: ${person.courses.join(", ")}</p>
+      <p>주소: ${person.address.city}, 우편번호: ${person.address.zipcode}</p>
+  </div>
+`
+  )
+  .join("");
+container.innerHTML = htmlContent;
