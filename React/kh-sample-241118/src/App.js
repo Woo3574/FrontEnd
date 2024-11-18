4e1397b84d3e645d43a5161084ca38d841469572
Woo3574 <rlaxodn0429@gmail.com>
2024-11-18 11:49:48 +0900

    24.11.18

--- React/kh-sample-241118/src/App.js
+++ React/kh-sample-241118/src/App.js
@@ -2,13 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    // 감싸인태그 <></> 리액트는 하나의 태그로 넘어가야한다
-    <>
-      <h1>리액트 공부를 시작하겠습니다.</h1>
-      <p>리액트는 JSX문법을 사용하고 리턴문내에서 작성 합니다.</p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
