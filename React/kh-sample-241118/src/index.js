2510c5a12237aa3237b6c7bf3d31032244a02059
Woo3574 <rlaxodn0429@gmail.com>
2024-11-19 09:47:31 +0900

    24.11.19

--- React/kh-sample-241118/src/index.js
+++ React/kh-sample-241118/src/index.js
@@ -10,8 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <App1118 />
-    <App1119 /> */}
+    {/* <App1118 />*/}
+    <App1119 />
   </React.StrictMode>
 );
 
