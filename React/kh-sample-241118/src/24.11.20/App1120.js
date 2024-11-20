92703efe1e5c9ccbba60fd1cf37bbba671e9e841
Woo3574 <rlaxodn0429@gmail.com>
2024-11-20 17:55:47 +0900

    24.11.20

--- React/kh-sample-241118/src/24.11.20/App1120.js
+++ React/kh-sample-241118/src/24.11.20/App1120.js
@@ -4,10 +4,11 @@ import UserList1 from "./UserList1";
 import TodoList from "./TodoList";
 import Submit from "./PracticeEx";
 import NameCard from "./PracticeExT";
+import InfoEffect from "./InfoEffect";
 function App1120() {
   return (
     <>
-      <Submit />
+      <InfoEffect />
     </>
   );
 }
--- React/kh-sample-241118/src/index.js
+++ React/kh-sample-241118/src/index.js
@@ -5,19 +5,18 @@ import App from "./App";
 import App1118 from "./24.11.18/App1118";
 import App1119 from "./24.11.19/App1119";
 import App1120 from "./24.11.20/App1120";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    {/* <App1118 />*/}
-    {/* <App1119 /> */}
-    <App1120 />
-  </React.StrictMode>
+  // <React.StrictMode></React.StrictMode>
+  //  <App />
+  // <App1118 />
+  //  <App1119 />
+  <App1120 />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
