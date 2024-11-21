3677b4b53af76e27f0376906b1eab7f7748fd279
Woo3574 <rlaxodn0429@gmail.com>
2024-11-21 14:31:33 +0900

    24.11.21

--- React/kh-style-241121/src/24.11.21/App1121.js
+++ React/kh-style-241121/src/24.11.21/App1121.js
@@ -1,9 +1,10 @@
 import "../App.css";
 import StyledComponent from "./StyledComponents";
-
+import GlobalStyle from "./GlobalStyle";
 function App1121() {
   return (
     <>
+      <GlobalStyle />
       <StyledComponent />
     </>
   );
