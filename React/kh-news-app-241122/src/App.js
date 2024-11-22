9dd3f3ceb594c7d396a9c7bba8c9269379bc25c7
Woo3574 <rlaxodn0429@gmail.com>
2024-11-22 14:50:30 +0900

    24.11.22

--- React/kh-news-app-241122/src/App.js
+++ React/kh-news-app-241122/src/App.js
@@ -1,8 +1,26 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=1d087979698d4b8ca3e9c262cfdd5373"
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
+    </div>
+  );
+};
 
 export default App;
---
+++ React/kh-news-app-241122/src/Components/NewsItems.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+const NewsItemBlock = styled.div`
+  display: flex;
+  .thumnail {
+  }
+`;
+
+const NewsItem = ({ article }) => {
+  const { title, description, url, urlToimage } = article;
+  return <></>;
+};
